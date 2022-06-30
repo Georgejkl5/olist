be063bb46beddf3ef6c1e9bc1b8aed7e27809f29
George Lee <george.lee159@gmail.com>
2022-06-30 10:20:07 +0800

    adding olist project to github

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Provide recommendations t to olist on how they should improve its profit margin',
+    author='George Lee',
+    license='MIT',
+)
